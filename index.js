8b834844f649678fc2465ca91d12a404ea26ce7e
saadafahmed45 <hafijnahid45@gmail.com>
2024-07-02 01:46:04 +0600

    added the cors

--- index.js
+++ index.js
@@ -119,6 +119,15 @@ async function run() {
       console.log();
     });
 
+    // Delete the first document in  collection
+
+    app.delete("/hotels/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete id ", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await hotelCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
